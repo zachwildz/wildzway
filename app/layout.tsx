5d932ee0b7dcf479090bd9a96c63a16cb43424bd
zachwildz <6p2hvqb678@privaterelay.appleid.com>
2026-02-26 14:59:32 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body style={{ margin: 0, padding: 0, overflow: 'hidden' }}>{children}</body>
     </html>
   )
 }
